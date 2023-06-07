ffa1e21e01ff4b947a532e59ea26dad5ddb2feb3
Tom Kelly <tom-kelly@hotmail.co.uk>
2023-06-07 11:58:52 +0100

    Added mapObject test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,17 @@
-import { pipe } from "../index";
-
-const sum = (a: number, b: number) => a + b;
-const times2 = (a: number) => a * 2;
-const toString = (a: number) => a.toString();
+import { mapObject, pipe } from "../index";
 
 test("Pipe function", () => {
+  const sum = (a: number, b: number) => a + b;
+  const times2 = (a: number) => a * 2;
+  const toString = (a: number) => a.toString();
+
   const o = pipe(sum(1, 1)).to(times2).to(toString).result;
   expect(o).toBe("4");
+});
+
+test("mapObject function", () => {
+  const a = ["a", "b", "c"];
+  const o: { [key: string]: string } = { a: "x", b: "y", c: "z", d: "none" };
+  const x = mapObject(a, (i) => [i, o[i]]);
+  expect(x).toStrictEqual({ a: "x", b: "y", c: "z" });
 });
